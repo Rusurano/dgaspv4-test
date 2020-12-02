b91fbc6f3bbe48bbaf446a4e4f69da0b800190a5
Duosora <ergi.duoson@gmail.com>
2020-12-02 23:39:53 +0200

    Updates

--- precache-manifest.b1756236ebec35715923fcc656a31f6c.js
+++ precache-manifest.b1756236ebec35715923fcc656a31f6c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa40e907bec70021babd82658a2f08c9",
+    "revision": "506c1472e40e805b3becc89046c40656",
     "url": "/dgaspv4-test/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dgaspv4-test/static/css/2.12795900.chunk.css"
   },
   {
-    "revision": "ed0975c1c01b5bfe0726",
-    "url": "/dgaspv4-test/static/css/main.4a30eb7c.chunk.css"
+    "revision": "faa2765d961c53fe3e53",
+    "url": "/dgaspv4-test/static/css/main.a0162ee4.chunk.css"
   },
   {
     "revision": "1e442d1b5f1a3a3d5a97",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dgaspv4-test/static/js/2.5ec00eba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed0975c1c01b5bfe0726",
-    "url": "/dgaspv4-test/static/js/main.83f737c5.chunk.js"
+    "revision": "faa2765d961c53fe3e53",
+    "url": "/dgaspv4-test/static/js/main.30a5e423.chunk.js"
   },
   {
     "revision": "f20a4986d4d62cc105e7",
